64fc5fd53b1d1d52575ff3996b29217a9173628b
lisaula <luis_isaula@hotmail.com>
2015-09-12 23:30:18 -0600

    LogicaBasica

---
+++ HSlider.gd
@@ -0,0 +1,14 @@
+
+extends HSlider
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Initialization here
+	pass
+
+
+
+
--- Nodo2D.gd
+++ Nodo2D.gd
@@ -4,9 +4,19 @@ extends Node2D
 # member variables here, example:
 # var a=2
 # var b="textvar"
-
+var cont=25
+var n
 func _ready():
-	# Initialization here
+	n=get_node("HSlider")
+
+
+
+
+func _on_TextureButton_pressed():
+	cont=cont+1
+	print(cont)
+	n.set_val(cont)
+
+
+func _on_HSlider_value_changed( value ):
 	pass
-
-
